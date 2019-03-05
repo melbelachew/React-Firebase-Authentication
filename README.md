237560243b2b5df3acdea658ccb4aa14ae8bb012
Mel Belachew <melbelachew@gmail.com>
2019-03-05 16:02:16 -0500

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# React-Firebase-Authentication
-# 
-Simple user sign-up/login/logout React-App using Firebase Authentication 
---
+++ precache-manifest.4cd6c716c3b80f70bad59bc3508d7a23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9623ae32bb94d88d327",
+    "url": "/static/css/main.504e3b48.chunk.css"
+  },
+  {
+    "revision": "f9623ae32bb94d88d327",
+    "url": "/static/js/main.f9623ae3.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9e0b47615636f5b7b331",
+    "url": "/static/js/2.9e0b4761.chunk.js"
+  },
+  {
+    "revision": "ea694491fc090a50d78bb4ed218ce54b",
+    "url": "/index.html"
+  }
+];
