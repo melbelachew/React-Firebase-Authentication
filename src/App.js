5c8247cb5507bf2e4e1ec51ffd642976126d96e9
Mel Belachew <melbelachew@gmail.com>
2019-03-05 15:21:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import fire from './config/Fire';
+import Home from './Home';
+import Login from './Login';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = ({
+      user:{}
+    });
+    this.authListner = this.authListner.bind(this);
+  }
+  componentDidMount(){
+    this.authListner();
+  }
+  authListner() {
+    fire.auth().onAuthStateChanged((user) => {
+     console.log(user);
+      if (user){
+        this.setState({user});
+        localStorage.setItem('user', user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+      }
+      });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+       {this.state.user ? (<Home/>) : (<Login/>)}
       </div>
     );
   }
